91cb75e047b1214c4dc31da9ff8aa95db2b0b883
David Whitlock <alovedalongthe@gmail.com>
2017-11-29 11:51:45 +0700

    Updated integration test

--- integration_test/phx_integration_test.sh
+++ integration_test/phx_integration_test.sh
@@ -3,7 +3,19 @@
 
 LOG="../../integration_test/phx_integration_test.log"
 
-function enter_cave {
+function setup_phx {
+    echo y | mix phx.new alibaba $@
+    cp -r alibaba alibaba_confirm
+    cd alibaba
+}
+
+function phauxth_project {
+    start_log $@
+    mix deps.get
+    run_tests
+}
+
+function start_log {
     echo -e "\nDATE: $(date) OPTIONS: $@\n" >> $LOG
     mix phauxth.new $@
 }
@@ -13,28 +25,23 @@ function run_tests {
     MIX_ENV=test mix ecto.drop
 }
 
-function clean {
-    cd ..
-    rm -rf alibaba
-}
-
-function phauxth_project {
-    cd alibaba || exit $?
-    enter_cave $@
-    mix deps.get
-    run_tests
-    clean
+function integration_test {
+    phauxth_project $@
+    cd .. && rm -rf alibaba
+    cd alibaba_confirm
+    phauxth_project $@ --confirm
+    cd .. && rm -rf alibaba_confirm
 }
 
 cd $(dirname "$0")/../tmp
-echo y | mix phx.new alibaba
-phauxth_project
-echo y | mix phx.new alibaba
-phauxth_project --confirm
-echo y | mix phx.new alibaba --no-html --no-brunch
-phauxth_project --api
-echo y | mix phx.new alibaba --no-html --no-brunch
-phauxth_project --api --confirm
+
+setup_phx
+integration_test
+setup_phx --no-html --no-brunch
+integration_test --api
+
 echo "------------------------------------------------------------"
+echo "Report for Phauxth integration test - $(date)"
 grep "test.*failure" ../integration_test/phx_integration_test.log
-grep "error" ../integration_test/phx_integration_test.log
+grep "error" ../integration_test/phx_integration_test.log || echo "No errors"
+echo "------------------------------------------------------------"
